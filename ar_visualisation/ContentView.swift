21ff9a24b877804d9f86921e894211fcd9d2ef1c
SIVANESAN S <itsmesiva057@gmail.com>
2026-03-19 08:32:41 +0530

    Initial Commit

---
+++ ar_visualisation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ar_visualisation
+//
+//  Created by SAIL L1 on 19/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ar_visualisation/ar_visualisationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ar_visualisationApp.swift
+//  ar_visualisation
+//
+//  Created by SAIL L1 on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ar_visualisationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
